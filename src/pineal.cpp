0368c380f95ad13ab94089419b03120c14023db3
kdorman <kenny.dorman@gmail.com>
2012-09-05 22:54:45 -0400

    Add default project elements + eclipse project file

---
+++ src/pineal.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : pineal.cpp
+// Author      : kdorman (kenny.dorman@gmail.com)
+// Version     :
+// Copyright   : GPL2 + CC
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
