cb0c7c63672a75c8b3f0d36e22f9b5ae94768ee7
kdorman <kenny.dorman@gmail.com>
2012-09-06 14:58:28 -0400

    make first build small modification to project file

--- src/pineal.cpp
+++ src/pineal.cpp
@@ -6,10 +6,32 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <allegro5/allegro.h>
+//#include <allegro5/allegro_native_dialog.h>
 
-int main() {
-	cout << "" << endl; // prints 
-	return 0;
+int main(void)
+{
+  ALLEGRO_DISPLAY *display = NULL;
+
+  if(!al_init())
+  {
+    //al_show_native_message_box(NULL, NULL, NULL, "failed to initialize allegro!", NULL, NULL);
+    return -1;
+  }
+
+  display = al_create_display(640, 480);
+
+
+  if(!display)
+  {
+    //al_show_native_message_box(NULL, NULL, NULL, "failed to initialize display!", NULL, NULL);
+    return -1;
+  }
+
+  sleep(5);
+
+  al_destroy_display(display);
+
+  return 0;
 }
