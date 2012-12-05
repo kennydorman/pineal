74868eaaaf1a4f451d16206f8f4798ee25217794
A. Jacques <andre.jacques@innoweb.ca>
2012-12-04 20:42:01 -0500

    Add operator + on position

--- src/includes/position.h
+++ src/includes/position.h
@@ -17,64 +17,81 @@ class Position {
 		 * Position X value
 		 */
 		double X;
+
 		/**
 		 * Position Y value
 		 */
 		double Y;
+
 	public:
 		/**
 		 * Zero out constructor
 		 */
 		Position() {
-			X = 0;
-			Y = 0;
+			this.X = 0;
+			this.Y = 0;
 		}
+
 		/**
 		 * basic position constructor
 		 */
-		Position(double _X, double _Y){
-			X = _X;
-			Y = _Y;
+		Position(double _X, double _Y)
+			this.X = _X;
+			this.Y = _Y;
 		}
+
 		/**
 		 * empty destructor :(
 		 */
 		virtual ~Position(){}
+
 		/**
 		 * X getter
 		 * @return double - X value
 		 */
 		double getX ( void ) {
-			return X;
+			return this.X;
 		}
+
 		/**
 		 * Y getter
 		 * @return double - Y value
 		 */
 		double getY ( void ) {
-			return Y;
+			return this.Y;
 		}
+
 		/**
 		 * X setter
 		 * @param double _X - new X value
 		 */
 		void setX ( double _X ) {
-			X = _X;
+			this.X = _X;
 		}
+
 		/**
 		 * Y setter
 		 * @param double _Y - new Y value
 		 */
 		void setY ( double _Y ) {
-			Y = _Y;
+			this.Y = _Y;
 		}
+
 		/**
 		 * Set position equal to an other one
 		 * @param Position _position - New position
 		 */
 		void setPosition ( Position* _position ) {
-			X = _position->getX();
-			Y = _position->getY();
+			this.X = _position->getX();
+			this.Y = _position->getY();
+		}
+
+		/**
+		 * Add two position (the x's together, the y's together) and output the result
+		 * @param Position _position - a position to add
+		 */
+		Position operator+ ( const Position& _position ) {
+			return new Position( this.X + _position.getX(), this.Y + _position.getY() );
 		}
 };
 
